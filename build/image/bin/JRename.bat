436e9b217b9a0a2a0991e4fd45484cfb0c1aa100
Sinaye Mgenge <43071489+terrificFrogg@users.noreply.github.com>
2022-01-24 18:55:24 +0200

    Delete build directory

--- build/image/bin/JRename.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set DIR="%~dp0"
-set JAVA_EXEC="%DIR:"=%\javaw"
-
-
-
-pushd %DIR% & start "com.sinaye.jrename" %JAVA_EXEC% %CDS_JVM_OPTS%  -p "%~dp0/../app" -m com.sinaye.jrename/com.sinaye.jrename.Start  %* & popd
