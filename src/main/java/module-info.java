04fc1a51311aebe9ef2188ce4dc5a1f885cdb7db
Sinaye Mgenge <43071489+terrificFrogg@users.noreply.github.com>
2022-03-26 17:59:44 +0200

    Added new feature that saves the last opened directory.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.sinaye.jrename {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.jsoup;
+    requires java.logging;
 
 
     opens com.sinaye.jrename to javafx.fxml;
