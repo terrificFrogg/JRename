69948d4f2ae92f90207a9230640d32b2122fd45d
Sinaye Mgenge <43071489+terrificFrogg@users.noreply.github.com>
2022-03-26 18:23:52 +0200

    Fixed imports in Settings.java

--- src/main/java/com/sinaye/jrename/Settings.java
+++ src/main/java/com/sinaye/jrename/Settings.java
@@ -1,6 +1,11 @@
 package com.sinaye.jrename;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
